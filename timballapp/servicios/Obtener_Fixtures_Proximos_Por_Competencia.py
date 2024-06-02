5f5d797801505e8d5756e7023cc7acfb70b669c2
Santino Andreatta <149499721+CuackXI@users.noreply.github.com>
2024-06-02 00:52:49 -0300

    skibidiiiii

--- timballapp/servicios/Obtener_Fixtures_Proximos_Por_Competencia.py
+++ timballapp/servicios/Obtener_Fixtures_Proximos_Por_Competencia.py
@@ -2,4 +2,4 @@ from ..models import *
 
 class Obtener_Fixtures_Proximos_Por_Competencia():
     def Obtener_Fixtures_Proximos_Por_Competencia(self, competencia):
-        return Fixture.objects.filter(IdApiComp=competencia).exclude(Status__in=["Match Postponed", "Match Finished", "Technical Loss", "WalkOver"]).order_by('Fecha', 'Hora')
+        return Fixture.objects.filter(IdApiComp=competencia).exclude(Status__in=["Match Postponed", "Match Finished", "Technical Loss", "WalkOver", "Time to be defined"]).order_by('Fecha', 'Hora')
